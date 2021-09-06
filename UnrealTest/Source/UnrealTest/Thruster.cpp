fb28445b625bb957119145ba6915de8fa158870c
SpookiG <laycockmichael@yahoo.co.uk>
2021-09-06 20:11:36 +0100

    Revert "Unreal started crashing so I'm commiting these changes to revert them back"
    This reverts commit b4c1fd7eda852ecf22bf651ab30d631b8454be4f.

--- UnrealTest/Source/UnrealTest/Thruster.cpp
+++ UnrealTest/Source/UnrealTest/Thruster.cpp
@@ -1,22 +1,30 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+
 #include "Thruster.h"
 #include "UObject/ConstructorHelpers.h"
 #include "Components/StaticMeshComponent.h"
-#include "PhysicsEngine/PhysicsConstraintComponent.h"
 #include "Engine/StaticMesh.h"
 
 // Sets default values for this component's properties
 UThruster::UThruster()
 {
-	
+	// Structure to hold one-time initialization
+	struct FConstructorStatics
+	{
+		ConstructorHelpers::FObjectFinderOptional<UStaticMesh> ThrusterMesh;
+		FConstructorStatics()
+			: ThrusterMesh(TEXT("/Game/StarterContent/Shapes/Shape_Cube"))
+		{
+		}
+	};
+	static FConstructorStatics ConstructorStatics;
 
-	
-
-	
-	//PhysicsConstraint->ConstraintActor1 = this;
-	//PhysicsConstraint->ComponentName1
-
+	// Create thruster mesh
+	ThrusterMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ThrusterMesh"));
+	ThrusterMesh->SetupAttachment(this);
+	ThrusterMesh->SetStaticMesh(ConstructorStatics.ThrusterMesh.Get());
+	ThrusterMesh->SetRelativeScale3D(FVector(0.25f, 0.25f, 0.25f));
 
 
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
@@ -24,39 +32,6 @@ UThruster::UThruster()
 	PrimaryComponentTick.bCanEverTick = true;
 
 	// ...
-}
-
-
-void UThruster::SetupPhysicsConstraint(AActor* ParentActor, FString ThrusterName, FString MeshName)
-{
-
-	
-	// Structure to hold one-time initialization
-	//struct FConstructorStatics
-	//{
-	//	ConstructorHelpers::FObjectFinderOptional<UStaticMesh> ThrusterMesh;
-	//	FConstructorStatics()
-	//		: ThrusterMesh(TEXT("/Game/StarterContent/Shapes/Shape_Cube"))
-	//	{
-	//	}
-	//};
-	//static FConstructorStatics ConstructorStatics;
-
-	// Create thruster mesh with custom name
-	//ThrusterMesh = CreateDefaultSubobject<UStaticMeshComponent>(*MeshName);
-	//ThrusterMesh->SetupAttachment(this);
-	//ThrusterMesh->SetStaticMesh(ConstructorStatics.ThrusterMesh.Get());
-	//ThrusterMesh->SetRelativeScale3D(FVector(0.25f, 0.25f, 0.25f));
-
-	// create and attach the physics constraint using the names
-	//PhysicsConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(TEXT("PhysicsConstraint"));
-	//PhysicsConstraint->SetupAttachment(this);
-	//PhysicsConstraint->ConstraintActor1 = ParentActor;
-	//PhysicsConstraint->ComponentName1.ComponentName = *ThrusterName;
-	//PhysicsConstraint->ConstraintActor2 = ParentActor;
-	//PhysicsConstraint->ComponentName2.ComponentName = *MeshName;
-
-	
 }
 
 
@@ -74,8 +49,6 @@ void UThruster::BeginPlay()
 void UThruster::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	ThrusterMesh->AddForce(FVector(50.f, 50.f, 50.f));
 
 	// ...
 }
